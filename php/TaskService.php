32dbd16e06f9fd19196ad6c5a60ce7db45adf4ed
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-06-11 17:36:57 -0300

    tasks adicionado

--- php/TaskService.php
+++ php/TaskService.php
@@ -1,16 +1,16 @@
 <?php
-include "Task.php";
+include_once "Task.php";
 
 /**
 * Service para Tarefas;
 */
 class TaskService
 {
-  private static $xmlFile = 'tasks.xml';
+  private static $xmlFile = 'php/tasks.xml';
   private $xml;
 
   function __construct() {
-    loadXmlFromFile();
+    $this->loadXmlFromFile();
   }
 
   private function loadXmlFromFile() {
@@ -23,8 +23,8 @@ class TaskService
     $newTask->title = $tarefa->getTitle();
     $newTask->date = $tarefa->getDate();
 
-    saveXmlAsFile();
-    loadXmlFromFile();
+    $this->saveXmlAsFile();
+    $this->loadXmlFromFile();
   }
 
   private function saveXmlAsFile() {
